27c348dbde8472fbab5aad8acf977e11215be6fc
HYivisir <843702140@qq.com>
2021-04-12 21:59:02 +0800

    添加一键分享

--- src/views/CustomerService/CustomerService.vue
+++ src/views/CustomerService/CustomerService.vue
@@ -28,35 +28,7 @@
         </ul>
       </div>
       <div class="chat-utils">
-        <emoji @emotion="handleEmotion" />
-        <!-- <div class="chat-icon">
-          <svg
-            t="1602854622107"
-            class="icon"
-            viewBox="0 0 1024 1024"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            p-id="4262"
-            width="25"
-            height="25"
-          >
-            <path
-              d="M191.802 833.049c-26.552 0-48.153-21.886-48.153-48.786v-548.08c0-26.901 21.602-48.786 48.153-48.786h204.061c13.065 0 25.741 5.482 34.776 15.041l47.123 49.853c18.447 19.508 44.365 30.694 71.112 30.694H845.5c26.55 0 48.149 21.884 48.149 48.784v452.494c0 26.9-21.6 48.786-48.149 48.786H191.802z"
-              fill="#FFFFFF"
-              p-id="4263"
-            />
-            <path
-              d="M395.863 212.397c6.221 0 12.274 2.629 16.609 7.215l47.129 49.858c23.145 24.477 55.684 38.515 89.275 38.515H845.5c12.765 0 23.15 10.67 23.15 23.784v452.494c0 13.116-10.385 23.786-23.15 23.786H191.802c-12.767 0-23.153-10.67-23.153-23.786v-548.08c0-13.116 10.386-23.786 23.153-23.786h204.061m0-50H191.802c-40.4 0-73.153 33.035-73.153 73.786v548.08c0 40.75 32.753 73.786 73.153 73.786H845.5c40.398 0 73.15-33.037 73.15-73.786V331.769c0-40.75-32.751-73.784-73.15-73.784H548.875c-20.007 0-39.137-8.266-52.944-22.868l-47.124-49.852c-13.803-14.604-32.937-22.868-52.944-22.868z"
-              fill="#1A1A1A"
-              p-id="4264"
-            />
-            <path
-              d="M168.66 391.447h556.523c19.21 0 34.783 15.572 34.783 34.783v139.13c0 19.209-15.572 34.783-34.783 34.783H168.66V391.447z"
-              fill="#FFCD36"
-              p-id="4265"
-            />
-          </svg>
-        </div>-->
+        <!-- <emoji @emotion="handleEmotion" /> -->
       </div>
       <div>
         <textarea
@@ -79,7 +51,7 @@
   </div>
 </template>
 <script>
-import Emoji from "./Emoji/Emoji";
+// import Emoji from "./Emoji/Emoji";
 import Message from "./Message/Message";
 import SockJs from "sockjs-client";
 import Stomp from "stompjs";
@@ -98,7 +70,7 @@ export default {
     };
   },
   components: {
-    Emoji,
+    // Emoji,
     Message,
   },
   computed: {
@@ -289,7 +261,7 @@ export default {
                 date.getMinutes() +
                 ":" +
                 date.getSeconds(),
-              content: theContent,
+              content: res.data.data.content,
             });
             this.content = "";
             document.getElementById("chat-area").focus();
@@ -315,10 +287,10 @@ export default {
       data.append("chat", localStorage.getItem("customer_chat"));
 
       this.$http
-        .post(this.domain + "chat/chatId", data,{
-          headers : {
-            token : "welcome"
-          }
+        .post(this.domain + "chat/chatId", data, {
+          headers: {
+            token: "welcome",
+          },
         })
         .then((res) => {
           if (res.data.code == 1) {
@@ -375,9 +347,7 @@ export default {
     getHistory() {
       if (this.serverId) {
         this.$http
-          .post(
-            this.domain + `chat/${this.clientId}/history/${this.serverId}`
-          )
+          .post(this.domain + `chat/${this.clientId}/history/${this.serverId}`)
           .then((res) => {
             console.log(res.data);
             let data = res.data.data;
@@ -415,9 +385,7 @@ export default {
     initWebSocket() {
       if (!window.socket) {
         let that = this;
-        let socket = new SockJs(
-          that.domain + "ws-websocket?" + this.clientId
-        );
+        let socket = new SockJs(that.domain + "ws-websocket?" + this.clientId);
         // 获取Stomp子协议的客户端对象
         window.socket = Stomp.over(socket);
         // 发起websocket连接
@@ -478,5 +446,9 @@ export default {
 <style>
 .ivu-modal-header {
   background: rgb(0, 160, 255);
+}
+.ivu-modal-header p,
+.ivu-modal-header-inner {
+  color: #fff !important;
 }
 </style>
